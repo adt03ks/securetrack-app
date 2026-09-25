16efdbff2cbbd95dd01a8226da40221e7a47a328
Angela Speights <Angela.Speights@PerryHomes.com>
2026-09-25 16:26:00 -0500

    Update daily-activity-report.js

--- daily-activity-report.js
+++ daily-activity-report.js
@@ -1,1 +1,91 @@
+(async function () {
 
+  "use strict";
+
+
+  const params =
+    new URLSearchParams(
+      window.location.search
+    );
+
+
+  const shiftInstanceId =
+    params.get(
+      "shiftInstanceId"
+    );
+
+
+  const shiftDate =
+    params.get(
+      "shiftDate"
+    );
+
+
+  const shiftName =
+    params.get(
+      "shiftName"
+    );
+
+
+  const returnTo =
+    params.get(
+      "returnTo"
+    );
+
+
+  document
+    .getElementById(
+      "shiftInstanceId"
+    )
+    .textContent =
+
+      shiftInstanceId
+      ||
+      "Not provided";
+
+
+  document
+    .getElementById(
+      "shiftDate"
+    )
+    .textContent =
+
+      shiftDate
+      ||
+      "Not provided";
+
+
+  document
+    .getElementById(
+      "shiftName"
+    )
+    .textContent =
+
+      shiftName
+      ||
+      "Not provided";
+
+
+  const returnLink =
+    document.getElementById(
+      "returnToAssignments"
+    );
+
+
+  if (
+    returnTo
+  ) {
+
+    returnLink.href =
+      returnTo;
+
+  }
+
+  else {
+
+    returnLink.href =
+      "./";
+
+  }
+
+})();
